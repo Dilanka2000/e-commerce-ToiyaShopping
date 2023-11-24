79a75cacebfc6b95d40388bc05acdbda602f2e32
Dilanka2000 <shdheshara20@gmail.com>
2023-11-24 13:08:33 +0530

    home page frontEnd finish

---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,20 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-filters',
+  standalone: true,
+  imports: [CommonModule, MatExpansionModule, MatListModule],
+  templateUrl: './filters.component.html',
+  styleUrl: './filters.component.css',
+})
+export class FiltersComponent {
+  @Output() showCategory = new EventEmitter<string>();
+  categories = ['Shoes', 'Sports'];
+
+  onShowCategory(category: string): void{
+    this.showCategory.emit(category);
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,12 +2,36 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ProductsHeaderComponent } from './components/products-header/products-header.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ProductBoxComponent } from './components/product-box/product-box.component';
+
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 4: 350, 6: 330 };
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, ProductsHeaderComponent],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    ProductsHeaderComponent,
+    FiltersComponent,
+    MatGridListModule,
+    ProductBoxComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  cols = 4;
+  rowHeight = ROWS_HEIGHT[this.cols];
+  category: string | undefined;
+
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+  }
+}
